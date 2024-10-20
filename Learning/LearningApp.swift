4d10bb46e8ad8b57af20ee5c8f27f3def3a97d92
shaden <shaden.wasaidi@gmail.com>
2024-10-20 16:39:24 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by shaden  on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
